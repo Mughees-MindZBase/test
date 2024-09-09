f2e10cbbab113959beeac57bf201e224433b0a97
mughees_codistan <mugheessarwar@codistan.org>
2024-09-09 10:15:16 +0500

    second commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/header";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,10 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className="bg-white "
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="bg-white ">
         <Header />
         <Navbar />
         {children}
